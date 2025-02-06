4acd3bc6595ebe122cd66f4500ef72ab02ad39e7
Raqhib <raqhib.ataillah@gmail.com>
2025-02-06 12:22:56 +0800

    Latihan membuat Banner,Kategori,dan Bagian Menu

--- JetCoffee/app/src/main/java/id/co/mondo/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/id/co/mondo/jetcoffee/MainActivity.kt
@@ -3,46 +3,129 @@
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.co.mondo.jetcoffee.model.Menu
+import id.co.mondo.jetcoffee.model.dummyBestSellerMenu
+import id.co.mondo.jetcoffee.model.dummyCategory
+import id.co.mondo.jetcoffee.model.dummyMenu
+import id.co.mondo.jetcoffee.ui.components.CategoryItem
+import id.co.mondo.jetcoffee.ui.components.MenuItem
+import id.co.mondo.jetcoffee.ui.components.Search
+import id.co.mondo.jetcoffee.ui.components.SectionText
 import id.co.mondo.jetcoffee.ui.theme.JetCoffeeTheme
 
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             JetCoffeeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                jetCoffeeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun jetCoffeeApp() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+    ) {
+        Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Banner(
+    modifier: Modifier = Modifier,
+){
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+//        modifier = modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+    ) {
+        items(dummyCategory, key = { it.textCategory }){
+            category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ){
+        items(listMenu, key = { it.title }){ menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun jetCoffeeAppPreview() {
     JetCoffeeTheme {
-        Greeting("Android")
+        jetCoffeeApp()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+ fun CategoryRowPreview(){
+     JetCoffeeTheme {
+         CategoryRow()
+     }
+ }
+
+@Composable
+@Preview(showBackground = true)
+fun MenuRowPreview(){
+    JetCoffeeTheme {
+        MenuRow(dummyMenu)
     }
 }
--- JetCoffee/app/src/main/java/id/co/mondo/jetcoffee/ui/theme/Theme.kt
+++ JetCoffee/app/src/main/java/id/co/mondo/jetcoffee/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package id.co.mondo.jetcoffee.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = LightCoffeeBrown,
+    secondary = LightCoffeeBrown,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = CoffeeBrown,
+    secondary = CoffeeBrown,
+    background = Color.White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
