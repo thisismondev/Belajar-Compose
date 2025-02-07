65aa5c95ebbf7be68fcff90d6bff1fcb39443073
Raqhib <raqhib.ataillah@gmail.com>
2025-02-07 11:17:18 +0800

    StatefulConvereter

--- MyTemperatureConverter/app/src/main/java/id/co/mondo/mytemperatureconverter/MainActivity.kt
+++ MyTemperatureConverter/app/src/main/java/id/co/mondo/mytemperatureconverter/MainActivity.kt
@@ -4,11 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.co.mondo.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +31,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyTemperatureConverterTheme {
-                    Greeting(name = "Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    StatefulTemperatureInput()
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StatefulTemperatureInput(modifier: Modifier = Modifier) {
+    var input by rememberSaveable { mutableStateOf("") }
+    var output by rememberSaveable { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = {
+                Text(stringResource(R.string.enter_celcius))
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
 }
+
+private fun convertToFahrenheit(celcius: String) =
+    celcius.toDoubleOrNull()?.let {
+        (it * 9/5) + 32
+    }.toString()
+
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun GreetingPreview() {
     MyTemperatureConverterTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            StatefulTemperatureInput()
+        }
     }
 }
