7d01332e26bdc87384ee6413a19a070873b00a20
Raqhib <raqhib.ataillah@gmail.com>
2025-02-24 23:08:28 +0800

    membuat Navigation pada Bottom Navigation

--- JetReward/app/src/main/java/id/co/mondo/jetreward/JetRewardApp.kt
+++ JetReward/app/src/main/java/id/co/mondo/jetreward/JetRewardApp.kt
@@ -11,12 +11,15 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import id.co.mondo.jetreward.ui.navigation.NavigationItem
 import id.co.mondo.jetreward.ui.navigation.Screen
@@ -70,6 +73,9 @@ private fun BottomBar(
     NavigationBar(
         modifier = modifier,
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -96,8 +102,15 @@ private fun BottomBar(
                     )
                 },
                 label = { Text(item.title) },
-                selected = false,
+                selected = currentRoute == item.screen.route,
                 onClick = {
+                    navController.navigate(item.screen.route){
+                        popUpTo(navController.graph.findStartDestination().id){
+                            saveState = true
+                        }
+                        restoreState = true
+                        launchSingleTop = true
+                    }
                 }
             )
         }
