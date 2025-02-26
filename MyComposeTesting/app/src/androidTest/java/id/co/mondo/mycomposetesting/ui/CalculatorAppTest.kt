160183e4c98f0d41089fa5523bf4ba29de20d6c0
Raqhib <raqhib.ataillah@gmail.com>
2025-02-26 13:40:53 +0800

    createandroidrule

---
+++ MyComposeTesting/app/src/androidTest/java/id/co/mondo/mycomposetesting/ui/CalculatorAppTest.kt
@@ -0,0 +1,33 @@
+package id.co.mondo.mycomposetesting.ui
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import id.co.mondo.mycomposetesting.ui.theme.MyComposeTestingTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CalculatorAppTest{
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp(){
+        composeTestRule.setContent {
+            MyComposeTestingTheme {
+                CalculatorApp()
+            }
+        }
+    }
+
+    @Test
+    fun calculate_area_of_rectangle_correct(){
+        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
+        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
+        composeTestRule.onNodeWithText("Hitung!").performClick()
+        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+    }
+
+}
