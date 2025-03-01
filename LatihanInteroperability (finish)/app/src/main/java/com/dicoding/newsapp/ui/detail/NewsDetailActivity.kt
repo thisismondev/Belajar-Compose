8f91759a1fa040735ecdd93b72b495e99eda6d55
Raqhib <raqhib.ataillah@gmail.com>
2025-03-01 09:38:15 +0800

    compose dalam activity

--- LatihanInteroperability (finish)/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
+++ LatihanInteroperability (finish)/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.dicoding.newsapp.ui.detail
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -8,15 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarState
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -25,17 +19,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
-import androidx.core.content.IntentCompat
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.IntentCompat.getParcelableExtra
 import com.dicoding.newsapp.R
 import com.dicoding.newsapp.data.local.entity.NewsEntity
-import com.dicoding.newsapp.databinding.ActivityNewsDetailBinding
 import com.dicoding.newsapp.ui.ViewModelFactory
 
 class NewsDetailActivity : AppCompatActivity() {
 
     private lateinit var newsDetail: NewsEntity
-//    private lateinit var binding: ActivityNewsDetailBinding
     private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
     private val viewModel: NewsDetailViewModel by viewModels {
         factory
@@ -43,13 +35,11 @@ class NewsDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityNewsDetailBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
 
-        newsDetail = IntentCompat.getParcelableExtra(intent, NEWS_DATA, NewsEntity::class.java) as NewsEntity
+        newsDetail = getParcelableExtra(intent, NEWS_DATA, NewsEntity::class.java) as NewsEntity
 
         setContent {
-            MaterialTheme{
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -61,37 +51,7 @@ class NewsDetailActivity : AppCompatActivity() {
                 }
             }
         }
-
-//        binding.topAppBar.title = newsDetail.title
-//
-//        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.action_bookmark -> {
-//                    viewModel.changeBookmark(newsDetail)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-
-//        viewModel.bookmarkStatus.observe(this) { status ->
-//            setBookmarkState(status)
-//        }
-//
-//        binding.webView.webViewClient = WebViewClient()
-//        binding.webView.loadUrl(newsDetail.url.toString())
-//
-//        viewModel.setNewsData(newsDetail)
     }
-
-//    private fun setBookmarkState(state: Boolean) {
-//        val menuItem = binding.topAppBar.menu.findItem(R.id.action_bookmark)
-//        if (state) {
-//            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmarked_white)
-//        } else {
-//            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmark_white)
-//        }
-//    }
 
     companion object {
         const val NEWS_DATA = "data"
@@ -102,7 +62,7 @@ class NewsDetailActivity : AppCompatActivity() {
 fun NewsDetailScreen(
     newsDetail: NewsEntity,
     viewModel: NewsDetailViewModel,
-){
+) {
     viewModel.setNewsData(newsDetail)
     val bookmarkStatus by viewModel.bookmarkStatus.observeAsState(false)
     NewsDetailContent(
@@ -123,7 +83,7 @@ fun NewsDetailContent(
     bookmarkStatus: Boolean,
     updateBookmarkStatus: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -137,20 +97,35 @@ fun NewsDetailContent(
                 actions = {
                     IconButton(onClick = updateBookmarkStatus) {
                         Icon(
-                            painter = if (bookmarkStatus){
+                            painter = if (bookmarkStatus) {
                                 painterResource(R.drawable.ic_bookmarked_white)
-                            }else{
+                            } else {
                                 painterResource(R.drawable.ic_bookmark_white)
                             },
-                            contentDescription = stringResource(R.string.save_bookmark)
+                            contentDescription = stringResource(R.string.save_bookmark),
                         )
                     }
                 }
             )
         },
-            modifier = modifier
-    ){ innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding).fillMaxSize())
+        modifier = modifier
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+            AndroidView(
+                factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        webViewClient = WebViewClient()
+                    }
+                },
+                update = {
+                    it.loadUrl(url)
+                }
+            )
+        }
     }
 }
 
